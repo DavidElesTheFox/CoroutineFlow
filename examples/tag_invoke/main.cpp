7fbd4a3e8edcc3c32e6b730bf1874b45f7a590c5
David Eles <eles.david.88@gmail.com>
2025-05-09 10:41:58 +0200

    Tag invoke and examples.

---
+++ examples/tag_invoke/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include <coroutine_flow/tag_invoke.hpp>
+
+namespace cf = coroutine_flow;
+
+struct MyCpo
+{
+};
+
+class SomeLogic
+{
+  public:
+    template <typename T>
+    int operator()(int a, const T& object) const
+    {
+      int x = 2 * a;
+      if constexpr (cf::is_tag_invocable<MyCpo, cf::tag_t<T>>())
+      {
+        x = cf::tag_invoke(MyCpo{}, object);
+      }
+      return x;
+    }
+};
+
+class Foo
+{
+  public:
+    friend int tag_invoke(MyCpo&&, const Foo& f) { return 5; }
+};
+
+int main()
+{
+  std::cout << "Start example.tag_invoke" << std::endl;
+  SomeLogic logic;
+  const int result = logic(20, Foo{});
+  const int result_without_tag = logic(20, 4);
+
+  std::cout << "Result: " << result << std::endl;
+  std::cout << "Result without tag: " << result_without_tag << std::endl;
+  return 0;
+}
--- include/coroutine_flow/task.hpp
+++ include/coroutine_flow/task.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <coroutine_flow/tag_invoke.hpp>
+
 namespace coroutine_flow
 {
-    class task
-    {
 
-    };
-}
+template <typename T>
+class task
+{
+  protected:
+  public:
+    void sync_wait();
+    void run_async();
+};
+} // namespace coroutine_flow
