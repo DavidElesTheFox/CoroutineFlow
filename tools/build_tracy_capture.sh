b9041f3c5643728890fbcce75081fc2bbc0bc9a1
David Eles <eles.david.88@gmail.com>
2025-06-03 13:21:53 +0000

    Polishing tools folder

--- tools/build_tracy_capture.sh
+++ tools/build_tracy_capture.sh
@@ -4,13 +4,15 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-SRC_DIR=${BUILD_DIR:-SCRIPT_DIR/../build/_deps/tracylib-src}
+SRC_DIR=${BUILD_DIR:-$SCRIPT_DIR/../build/_deps/tracylib-src}
 
 echo "# Create directory: capture-build"
-mkdir capture-build
+mkdir -p capture-build
 
 echo "# Configure capture"
-cmake -S"$SRC_DIR/capture" -Bcature-build
+cmake -S"$SRC_DIR/capture" -B$SCRIPT_DIR/capture-build
 
 echo "# Build capture"
-cmake --build capture-build
+cmake --build $SCRIPT_DIR/capture-build
+
+ln -s $SCRIPT_DIR/capture-build/tracy-capture $SCRIPT_DIR/bin/tracy-capture
